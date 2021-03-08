23f1666033665b3f330d65ce1eac1afc9fd76eeb
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-03-08 23:11:15 +0900

    make janken game

---
+++ janken.py
@@ -0,0 +1,19 @@
+import random
+
+def play() :
+    user = input("what's your choice? choki for c, goo for g, paa for p\n")
+    computer = random.choice(['c', 'g', 'p'])
+
+    if user == computer:
+        return 'aiko!!!!'
+
+    if judge_winner(user, computer):
+        return "You Win!!!"
+
+    return "You Lose!!!"    
+
+def judge_winner(player, opponent):
+    if (player == 'c' and opponent == 'p') or (player == 'p' and opponent == 'g') or (player =='g' and opponent == 'c'):
+       return True
+
+print(play())
